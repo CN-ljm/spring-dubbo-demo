4f446fdf652cf3927761f35e97155bd7ca618718
jiamingl <759756407@qq.com>
2023-12-02 21:42:04 +0800

    微信服务验证代码

--- demo-service-second/src/main/java/com/ljm/controller/wechat/OfficialAccountsController.java
+++ demo-service-second/src/main/java/com/ljm/controller/wechat/OfficialAccountsController.java
@@ -1,9 +1,19 @@
 package com.ljm.controller.wechat;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 /**
  * @author Created by liangjiaming on 2023/6/28
@@ -11,12 +21,53 @@ import org.springframework.web.bind.annotation.RestController;
  * @Desc
  */
 @RestController
-@RequestMapping("wx/OfficialAccount")
+@RequestMapping("wx/")
 @Slf4j
 public class OfficialAccountsController {
 
-    @PostMapping("/autoReply")
-    public String autoReply(String msg) {
+    /**
+     * 微信服务器认证 token=ljm123456
+     * @param request
+     * @param response
+     */
+    @GetMapping("/receive")
+    public void getReceive(ServletRequest request, ServletResponse response) {
+        log.info("getReceive");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        log.debug("timestamp:{},nonce:{},echostr:{},signature:{}", timestamp, nonce, echostr, signature);
+        Set<String> sortSet = new HashSet<>();
+        sortSet.add(timestamp);
+        sortSet.add(nonce);
+        sortSet.add(echostr);
+        StringJoiner joiner = new StringJoiner("");
+        sortSet.forEach(s -> joiner.add(s));
+        // 计算签名
+        String signStr = getShaString(joiner.toString());
+        if (signature.equals(signStr) ) {
+            log.info("验签成功！");
+        } else {
+            log.info("验签不通过！");
+        }
+        String resStr = echostr;
+        PrintWriter writer = null;
+        try {
+            response.getWriter();
+            writer.write(resStr);
+            writer.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    @PostMapping("/receive")
+    public void PostReceive(String msg) {
         log.info("request message:{}", msg);
         String answer = "<xml>\n" +
                 " <ToUserName><![CDATA[粉丝号]]></ToUserName>\n" +
@@ -25,7 +76,24 @@ public class OfficialAccountsController {
                 " <MsgType><![CDATA[text]]></MsgType>\n" +
                 " <Content><![CDATA[hello!]]></Content>\n" +
                 "</xml>";
-        return answer;
+    }
+
+    /**
+     * sha1哈希计算
+     * @return
+     */
+    private String getShaString(String str) {
+        MessageDigest sha = null;
+        try {
+            sha = MessageDigest.getInstance("SHA");
+            byte[] digest = sha.digest(str.getBytes());
+            // 二进制转16进制
+            return Hex.encodeHexString(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
